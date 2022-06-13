98ec244687c48539c9ef3ce94963e9a4e1f16a45
William Abreu <weabreu16@gmail.com>
2022-06-13 19:14:27 -0400

    feat: avg math_utils method added

--- tests/math_utils.test.js
+++ tests/math_utils.test.js
@@ -7,3 +7,7 @@ test('add 1 + 5 to be 6', () => {
 test('add 6 + 5 + 10 to be 21', () => {
     expect( math_utils.sum(6, 5, 10) ).toBe(21);
 });
+
+test('avg 1, 3 to be 2', () => {
+    expect( math_utils.avg(1, 3) ).toBe(2);
+});
